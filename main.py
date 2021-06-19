818c515a8df8c4853358727e5246ba7d29a1e3fc
AaditiPradeep <63897431+AaditiPradeep@users.noreply.github.com>
2021-06-19 17:53:00 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import csv
 
 df = pd.read_csv("final.csv")
 
-del df["Luminosity"]
+df.drop(['Unnamed: 0','Unnamed: 6', 'Star_name.1', 'Distance.1', 'Mass.1', 'Radius.1','Luminosity'],axis=1,inplace=True)
 
-df.dropna()
+file_data = df.dropna()
+file_data.reset_index(drop=True,inplace = True)
 
-df.to_csv("main.csv")
+file_data.to_csv("main.csv")
